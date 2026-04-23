80137bdec19d974206d211d343491f035126088d
aaron <aaronchirag07@gmail.com>
2026-04-23 16:14:20 +0530

    multimediahall_aron

---
+++ frontend/multimediahall_aron/hallbooking/src/main.jsx
@@ -0,0 +1,10 @@
+import './index.css'; // This connects the "paint" to your "house"
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
---
+++ frontend/multimediahall_aron/hallbooking/tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // This tells Tailwind to style all your React files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
